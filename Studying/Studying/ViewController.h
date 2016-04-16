7c730bae1940bc785ffd25e0f4191a1792fc2dfe
lfc <1415683500@qq.com>
2016-04-16 14:18:14 +0800

    新建初始工程

---
+++ Studying/Studying/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Studying
+//
+//  Created by lfc on 16/4/16.
+//  Copyright © 2016年 HLYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Studying/Studying/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Studying
+//
+//  Created by lfc on 16/4/16.
+//  Copyright © 2016年 HLYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
